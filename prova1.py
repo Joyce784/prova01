d04ab00bfbc5e787204b4e488ab1e7825b5d9824
Joyce784 <joycesr.mj@gmail.com>
2024-11-06 11:54:09 -0300

    Add files via upload

---
+++ prova1.py
@@ -0,0 +1,7 @@
+usuario1 = int(input("Forneça uma base para um retangulo:")) #largura
+usuario2 = int(input("Forneça uma altura para um retangulo")) #comprimento
+
+retangulo = usuario1 * usuario2
+
+print(f"A area é {retangulo}")
+
